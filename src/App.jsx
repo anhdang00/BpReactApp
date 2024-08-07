39bea56a28e4646cb3e1bacfb26a60705977e037
anhdang00 <adang7@hawk.iit.edu>
2024-08-06 23:31:46 -0500

    create & style game board

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,28 @@ import './App.css'
 function App() {
   const TIC_TAC_TOE = new Array(3).fill(new Array(3).fill(null))
 
-  return <div>Tic Tac Toe</div>
+  const [boardState, setBoardState] = useState(TIC_TAC_TOE)
+
+  return <div>
+    <h1>Tic Tac Toe</h1>
+    {TIC_TAC_TOE.map(
+      (row,i) => {
+        return row.map((cell,j) =>{
+          return <div className='cell' onClick={() =>{
+            setBoardState(bs=>{
+              return [
+                ...bs.slice(0,i),
+                [...bs[i].slice(0,j),'X',...bs[i].slice(j+1)],
+                ...bs.slice(i+1)]
+            })
+          }}></div>
+        })
+        
+      }
+
+    )}
+    
+  </div>
 }
 
 export default App
