3ba3dc93bbddfb0fde8a4cdf47f35fb04a28d32e
anhdang00 <adang7@hawk.iit.edu>
2024-08-07 07:51:24 -0500

    add function to calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ function App() {
   const [isXNext,  setIsXNext]=useState(true)
 
   const displayWinner = useMemo(()=>{
+    const xRows=[], oRows[];
+
+    boardState.map((row,i)=>{
+      row.map((c,j)=>{
+
+      })
+    })
     return 'X'
   },[])
   return <div>
@@ -47,5 +54,26 @@ function App() {
 
     
 }
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 
 export default App
