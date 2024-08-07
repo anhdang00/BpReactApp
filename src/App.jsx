36813888797d170919ddd67be4155088d2394957
anhdang00 <adang7@hawk.iit.edu>
2024-08-06 23:12:10 -0500

    create a 3x3 array

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  const TIC_TAC_TOE = new Array(3).fill(new Array(3).fill(null))
+
+  return <div>Tic Tac Toe</div>
 }
 
 export default App
