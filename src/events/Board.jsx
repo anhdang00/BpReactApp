3fb2e108f1963fe93fb1fff4ff47da4968362a93
anhdang00 <adang7@hawk.iit.edu>
2024-08-12 14:58:37 -0500

    create seperate file for func Board()

---
+++ src/events/Board.jsx
@@ -0,0 +1,33 @@
+export default function Board(){
+    const styleObj={
+        display:'flex',
+        flexFlow:'row wrap',
+        width:'calc(var(--cellSize)*3)',
+        height:'calc(var(--cellSize)*3)'
+      }
+      
+    return (
+        <div style={styleObj}>
+      {TIC_TAC_TOE.map((row,i) => {
+        return row.map((cell,j) =>{
+          return (
+            <Cell 
+              key={`${i} - ${j}`}
+              onClick={() =>{
+                setBoardState(bs=>{
+                  const symbolToPut= isXNext?'X':'O';
+                  setIsXNext(!isXNext)
+                  return [
+                    ...bs.slice(0,i),
+                    [...bs[i].slice(0,j),symbolToPut,...bs[i].slice(j+1)],
+                    ...bs.slice(i+1)]
+                  })
+                }}
+                    symbol={boardState[i][j]}
+            /> 
+          )      
+        })       
+      })}
+    </div>
+    )
+}
