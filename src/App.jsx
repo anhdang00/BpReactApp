417ac99e39f49f7ceb9b4fe7ec80890fc29beeb6
anhdang00 <adang7@hawk.iit.edu>
2024-08-12 14:36:38 -0500

    add key

--- src/App.jsx
+++ src/App.jsx
@@ -36,21 +36,25 @@ function App() {
     <h1>Tic Tac Toe</h1>
     <h3>Winner is {displayWinner}</h3>
     <div style={styleObj}>
-      {TIC_TAC_TOE.map(
-      (row,i) => {
+      {TIC_TAC_TOE.map((row,i) => {
         return row.map((cell,j) =>{
-          return <div className='cell' onClick={() =>{
-            setBoardState(bs=>{
-              const symbolToPut= isXNext?'X':'O';
-              setIsXNext(!isXNext)
-              return [
-                ...bs.slice(0,i),
-                [...bs[i].slice(0,j),symbolToPut,...bs[i].slice(j+1)],
-                ...bs.slice(i+1)]
-            })
-          }}>
-            {boardState[i][j]}
-          </div>
+          return (
+            <div 
+              key={`${i} - ${j}`}
+              className='cell' 
+              onClick={() =>{
+                setBoardState(bs=>{
+                  const symbolToPut= isXNext?'X':'O';
+                  setIsXNext(!isXNext)
+                  return [
+                    ...bs.slice(0,i),
+                    [...bs[i].slice(0,j),symbolToPut,...bs[i].slice(j+1)],
+                    ...bs.slice(i+1)]
+                })
+              }}>
+                {boardState[i][j]}
+            </div>
+          )
         })
         
       }
