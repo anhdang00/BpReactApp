19fec9b1fb455302265eafb4919b6767af2f7f4d
anhdang00 <adang7@hawk.iit.edu>
2024-08-13 10:43:40 -0500

    add Lobby

--- src/events/TicTacToe/Cell.jsx
+++ src/events/TicTacToe/Cell.jsx
@@ -1,8 +1,8 @@
 
 
 export default function Cell( {onClick, symbol, ...props}){
-  return <div className='cell' onClick={onClick}>
-        {symbol}
-      </div>
+    return <div className='cell' onClick={onClick}>
+          {symbol}
+        </div>
 
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Game from './events/TicTacToe/Game'
+
+import Game from './Events/TicTacToe/Game'
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { createBrowserRouter,RouterProvider, useNavigate } from 'react-router-dom'
 
+const Lobby = ()=>{
+  const navigate = useNavigate()
+  return <div>Lobby
+    <button onClick={()=>{
+      navigate('/newSession')
+    }}> take me to the game</button>
+  </div>
+}
 
 const router = createBrowserRouter([
-  {path:'/', element:<Game></Game>}
+  {path:'/:sessionID', element:<Game></Game>},
+  {path:'/lobby', element: <Lobby/>},
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
