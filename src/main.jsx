bec465043575ebb43e29cf02cb9772f550e4318f
anhdang00 <adang7@hawk.iit.edu>
2024-08-13 15:47:33 -0500

    remove extra

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import Game from './Events/TicTacToe/Game'
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { createBrowserRouter,RouterProvider, useNavigate } from 'react-router-dom'
+import Lobby from './Lobby'
+
 
 
 const router = createBrowserRouter([
-  {path:'/', element:<Game></Game>}
+  {path:'/:sessionID', element:<Game></Game>},
+  {path:'/lobby', element: <Lobby/>},
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
